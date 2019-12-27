f5ad24bd3a40fb4fe3414e9e6b7c54fbc63adf6e
Yura-by <s.older681@gmail.com>
2019-12-27 11:57:15 +0300

    fixbug счетчика дней

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {generateEvents} from './mock/event.js';
 import {RenderPosition, render} from './utils/render.js';
 import TripController from './controller/trip.js';
 
-const EVENT_COUNT = 20;
+const EVENT_COUNT = 22;
 
 const siteHeaderElement = document.querySelector(`.page-header`);
 const siteContolsElement = siteHeaderElement.querySelector(`.trip-controls`);
